efbf92f69fd9cdbea5f8f5fee53b09cf479fe778
Guedes <paulo.guedes@vistajet.com>
2017-11-12 17:13:14 -0200

    rename and weservice

--- TreinamentoBalizador-IFSP/Utils/Constant.cs
+++ TreinamentoBalizador-IFSP/Utils/Constant.cs
@@ -8,6 +8,6 @@ namespace TreinamentoBalizador_IFSP.Utils
 {
     class Constant
     {
-        public const String URL = "http://192.168.100.9:8080/";
+        public const String URL = "http://ec2-18-217-16-72.us-east-2.compute.amazonaws.com:8080/";
     }
 }
